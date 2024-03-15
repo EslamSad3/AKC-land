4363e35daf99055d0fd51926ecc66098f02c8ce4
EslamSad3 <eslam.sa3d96@mail.com>
2024-03-16 00:35:43 +0200

    final1

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 // Initialization for ES Users
-import { Collapse, Dropdown,Carousel ,Ripple,initTWE } from "./tw-elements.es.min.js";
+import {
+  Collapse,
+  Dropdown,
+  Carousel,
+  Ripple,
+  initTWE,
+} from "./tw-elements.es.min.js";
 
-initTWE({ Collapse, Dropdown,Carousel,Ripple  });
+initTWE({ Collapse, Dropdown, Carousel, Ripple });
+
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 4,
+  spaceBetween: 5,
+  // freeMode: true,
+  Infinity: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
